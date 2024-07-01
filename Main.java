918e1f3afbcb9627119e4b9ea9415c4da823cd35
Мария <112629090+CatTichon@users.noreply.github.com>
2024-07-01 11:50:14 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем экземпляр окна
+        prototip dialog = new prototip();
+        // Настраиваем размер окна (опционально, если размер не установлен в самом GUI)
+//        dialog.pack();
+        dialog.setSize(600, 600);
+        // Делаем окно видимым
+        dialog.setVisible(true);
+        // Завершаем программу при закрытии окна (если это необходимо)
+        System.exit(0);
+    }
+}
