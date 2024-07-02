689ab52a5337a232fbac0f57cf9b56675d120316
Мария <112629090+CatTichon@users.noreply.github.com>
2024-07-02 18:21:29 +0300

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,28 @@
+
+import java.awt.*;
+
+    public class Node {
+        private Point point;
+        private int radius = 10;
+
+        public Node(int x, int y) {
+            this.point = new Point(x, y);
+        }
+
+        public Point getPoint() {
+            return point;
+        }
+
+        public int getRadius() {
+            return radius;
+        }
+
+        public void setPoint(int x, int y) {
+            this.point.setLocation(x, y);
+        }
+
+        public boolean contains(int x, int y) {
+            return point.distance(x, y) <= radius;
+        }
+    }
+
